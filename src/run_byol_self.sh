2414f01974b36bc31d20c3870b827fb397055f0a
sanyald <deepayan.sanyal@vanderbilt.edu>
2021-10-06 18:41:27 -0500

    Default run files for byol experiments

--- src/run_byol_self.sh
+++ src/run_byol_self.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-epochs=15
+epochs=20
+frac=0.02
+b_size=64
+workers=8
 
-python ssl_impl.py -d self -data toybox -f1 0.02 -f2 0.1 -lr 0.1 -lr_ft 0.2 -e1 $epochs -e2 5 -ssl byol -rep 1
+set -x
 
-python ssl_impl.py -d self -data toybox -f1 0.02 -f2 0.1 -lr 0.2 -lr_ft 0.2 -e1 $epochs -e2 5 -ssl byol -rep 1
+python ssl_impl.py -d self -data toybox -f1 $frac -f2 0.1 -lr 0.2 -lr_ft 0.2 -b $b_size -w $workers -e1 $epochs -e2 25 -ht -ssl byol -rep 1 -sv -sn byol_self_ht_1 -sr 20 -ef 20
+
+python ssl_impl.py -d self -data toybox -f1 $frac -f2 0.1 -lr 0.5 -lr_ft 0.2 -b $b_size -w $workers -e1 $epochs -e2 25 -ht -ssl byol -rep 1 -sv -sn byol_self_ht_2 -sr 20 -ef 20
+
+python ssl_impl.py -d self -data toybox -f1 $frac -f2 0.1 -lr 0.8 -lr_ft 0.2 -b $b_size -w $workers -e1 $epochs -e2 25 -ht -ssl byol -rep 1 -sv -sn byol_self_ht_3 -sr 20 -ef 20
---
+++ src/run_byol_transform.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+epochs=20
+frac=0.02
+b_size=64
+workers=4
+
+set -x
+
+python ssl_impl.py -d transform -data toybox -f1 $frac -f2 0.1 -lr 0.2 -lr_ft 0.2 -b $b_size -w $workers -e1 $epochs -e2 25 -ht -ssl byol -rep 1 -sv -sn byol_transform_ht_1 -sr 20 -ef 20
+
+python ssl_impl.py -d transform -data toybox -f1 $frac -f2 0.1 -lr 0.5 -lr_ft 0.2 -b $b_size -w $workers -e1 $epochs -e2 25 -ht -ssl byol -rep 1 -sv -sn byol_transform_ht_2 -sr 20 -ef 20
+
+python ssl_impl.py -d transform -data toybox -f1 $frac -f2 0.1 -lr 0.8 -lr_ft 0.2 -b $b_size -w $workers -e1 $epochs -e2 25 -ht -ssl byol -rep 1 -sv -sn byol_transform_ht_3 -sr 20 -ef 20
